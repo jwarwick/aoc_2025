c9d3d2c986f450eca87de1544512e70ce8b720b3
John Warwick <jwarwick@gmail.com>
2025-12-31 18:06:49 -0500

    Day 6, Part 2

--- src/day06.py
+++ src/day06.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from helpers import read_lines 
 import math
+from collections import defaultdict
 
 INPUT_PATH = Path(__file__).with_suffix(".txt")
 
@@ -10,6 +11,26 @@ def parse(lines: list[str]):
     elts = list(zip(*elts))
     return elts
 
+def parse2(lines: list[str]):
+    ops = lines[-1].split()
+    idx_list = [idx for idx, ch in enumerate(lines[-1]) if not ch.isspace()][1:]
+    idx_list.insert(0, 0)
+    idx_list.append(len(lines[-1])+2)
+    pairs = list(zip(idx_list, idx_list[1:]))
+    values = defaultdict(list)
+    for idx, pos in enumerate(pairs):
+        start, stop = pos
+        for l in lines[:-1]:
+            s = l[start:stop-1]
+            values[idx].append(s)
+    data = []
+    for k in values.keys():
+        l = values[k]
+        new_l = list(zip(*l))
+        new_l = list(map(lambda x: int("".join(x)), new_l))
+        new_l.append(ops[k])
+        data.append(new_l)
+    return data
 
 def part1(data) -> int:
     result = 0
@@ -23,16 +44,13 @@ def part1(data) -> int:
     return result
 
 
-def part2(data) -> int:
-    return 0
-
-
 def solve(path: Path = INPUT_PATH) -> None:
     """Read input, solve both parts, print results."""
     lines = read_lines(path)
     data = parse(lines)
     print("Part 1:", part1(data))
-    print("Part 2:", part2(data))
+    data2 = parse2(lines)
+    print("Part 2:", part1(data2))
 
 
 if __name__ == "__main__":
@@ -56,6 +74,6 @@ def test_example_part1():
 
 
 def test_example_part2():
-    data = parse(_example_lines())
-    assert part2(data) == 3263827
+    data = parse2(_example_lines())
+    assert part1(data) == 3263827
 
